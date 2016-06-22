b2fea42284ce4a04e6744451e0e73052fbec4381
Ev1lbl0w <ricjssubtil@yahoo.com>
2016-06-22 18:29:01 +0100

    Initial work on the game
    - Created 2 stages: Stage_1 and The_Valley
    - Created a tree model and respetive materials
    - Debugging on Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,20 @@
 package main;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.scene.BatchNode;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.terrain.Terrain;
+import com.jme3.texture.Texture;
+import com.jme3.util.SkyFactory;
 
 /**
  * test
@@ -21,14 +29,50 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        Node level1 = (Node)assetManager.loadModel("Scenes/Stage_1.j3o");
+        rootNode.attachChild(level1);
+        Terrain terrain = (Terrain)level1.getChild("Terrain");
+        
+        flyCam.setMoveSpeed(30f);
+        
+        /** A white ambient light source. */ 
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        rootNode.addLight(ambient); 
+        
+        /** A white, directional light source */ 
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun); 
+    
+        FilterPostProcessor fpp = assetManager.loadFilter("Interface/waterFilter.j3f");
+        viewPort.addProcessor(fpp);
+        
+        //viewPort.setBackgroundColor(new ColorRGBA(135/255f, 206/255f, 235/255f, 1));
+        Texture skyBox = assetManager.loadTexture("Textures/skybox.png");
+        Spatial sky = SkyFactory.createSky(assetManager, skyBox, skyBox, skyBox, skyBox, skyBox, skyBox);
+        rootNode.attachChild(sky);
+        
+        BatchNode trees = new BatchNode("Trees");
+        for(int i = 0; i < 100; i++) {
+            int x = FastMath.nextRandomInt(-80, 80);
+            int z = FastMath.nextRandomInt(-80, 80);
+            float y = terrain.getHeight(new Vector2f(x, z)) - 135;
+            
+            if(y < 1) {
+                i -= 1;
+                continue;
+            }
+            
+            Node tree = (Node)assetManager.loadModel("Textures/trees/tree1.j3o");
+            tree.setLocalTranslation(x, y, z);
+            trees.attachChild(tree);
+        }
+        
+        trees.batch();
+        rootNode.attachChild(trees);
+        System.out.println(trees.getChildren().size());
     }
 
     @Override
